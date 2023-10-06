c0a49061bc63fbcf36c4c842af70df133580f8e9
Qin Guan <qinguan20040914@gmail.com>
2023-10-06 09:31:36 +0800

    feat(backend): add proxyable paths

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,15 +8,43 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+/**
+ * Check feature flags on GB to determine which paths are proxyable, etc
+ */
+async function getProxyablePaths() {
+	const defaultProxyable = [
+		'/environment/2-hour-weather-forecast'
+	]
+
+	return [...defaultProxyable]
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // const globalPrefix = 'api';
-  // app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}`
-  );
+	const app = await NestFactory.create(AppModule);
+	// const globalPrefix = 'api';
+	// app.setGlobalPrefix(globalPrefix);
+
+	const proxyablePaths = await getProxyablePaths()
+
+	app.use(
+		'/',
+		createProxyMiddleware({
+			target: 'https://api-staging.data.gov.sg',
+			changeOrigin: true,
+			secure: true,
+			pathFilter(path) {
+				return proxyablePaths.includes(path)
+			}
+		})
+	);
+
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
+	Logger.log(
+		`🚀 Application is running on: http://localhost:${port}`
+	);
 }
 
 bootstrap();
